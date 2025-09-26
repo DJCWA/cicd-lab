1dc06b60961e81d104acdb732da08011b127dc7b
DJCWA <kdewsh69@gmail.com>
2025-09-25 20:43:09 -0400

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -9,4 +9,3 @@ aws s3 cp s3://test-webappdeploymentbucket-jovalhdbibun/SpringBootHelloWorldExam
 # Ensure the ownership permissions are correct.
 chown -R tomcat:tomcat /usr/local/tomcat9/webapps
 
-systemctl restart tomcat.service
