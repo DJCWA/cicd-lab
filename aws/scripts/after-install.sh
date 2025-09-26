391949a0f5fe9a473667c05f334198febd46c9d6
DJCWA <kdewsh69@gmail.com>
2025-09-25 21:09:22 -0400

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://test-webappdeploymentbucket-jovalhdbibun/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
-
+aws s3 cp s3://test-webappdeploymentbucket-jovalhdbibun/ROOT.war /usr/local/tomcat9/webapps/ROOT.war
 
 # Ensure the ownership permissions are correct.
 chown -R tomcat:tomcat /usr/local/tomcat9/webapps
